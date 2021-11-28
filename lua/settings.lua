1164222f037b225dcb53bf60b5a7978db2eaeb2e
Mafuuw <katzeyurei@gmail.com>
2021-11-28 22:55:01 +0800

    Yes

--- lua/settings.lua
+++ lua/settings.lua
@@ -3,16 +3,17 @@ local g = vim.g
 local cmd = vim.cmd
 
 
-o.splitright = true       -- Splits open on the right
-o.number = true           -- Enable line numbers.
-o.expandtab = true        -- Convert tabs to spaces.
-o.mouse = 'a'             -- Enable mouse.
-o.wildmenu = true         -- Enable a menu (Wildmenu) at the bottom of the nvim window.
-o.hlsearch = true         -- Enable search highlighting.
-o.ignorecase = true       -- Ignore case when searching.
-o.smartcase = true        -- Automatically switch search to case-sensitive when search query contains an uppercase letter.
-o.shiftwidth = 2          -- When shifting, indent using four spaces.
-o.cursorline = true       -- Highlight the current line
+o.splitright = true           -- Splits open on the right
+o.number = true               -- Enable line numbers.
+o.expandtab = true            -- Convert tabs to spaces.
+o.mouse = 'a'                 -- Enable mouse.
+o.wildmenu = true             -- Enable a menu (Wildmenu) at the bottom of the nvim window.
+o.hlsearch = true             -- Enable search highlighting.
+o.ignorecase = true           -- Ignore case when searching.
+o.smartcase = true            -- Automatically switch search to case-sensitive when search query contains an uppercase letter.
+o.shiftwidth = 2              -- When shifting, indent using four spaces.
+o.cursorline = true           -- Highlight the current line
+o.clipboard = 'unnamedplus'   -- Use system clipboard
 o.updatetime = 300
 o.foldlevelstart = 99
 o.tabstop = 2 
