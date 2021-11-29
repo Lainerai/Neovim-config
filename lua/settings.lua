b02e8050afc0c34ce66e7667466b669e0e2abd8c
Mafuuw <katzeyurei@gmail.com>
2021-11-29 21:49:36 +0800

    I Forgor💀

--- lua/settings.lua
+++ lua/settings.lua
@@ -14,10 +14,17 @@ o.smartcase = true            -- Automatically switch search to case-sensitive w
 o.shiftwidth = 2              -- When shifting, indent using four spaces.
 o.cursorline = true           -- Highlight the current line.
 o.clipboard = 'unnamedplus'   -- Use system clipboard.
-o.tabstop = 2                 -- number of columns occupied by a tab
-o.softtabstop = 0             -- see multiple spaces as tabstops so <BS>
+o.tabstop = 2                 -- number of columns occupied by a tab.
+o.softtabstop = 0             -- see multiple spaces as tabstops so <BS>.
+o.wrap = false                -- Display long lines as just one line.
+o.scrolloff = 12              -- determines the number of context lines you would like to see above and below the cursor.
+o.list = true
+o.timeoutlen = 3000
+o.ttimeoutlen = 100
 o.updatetime = 300        
 o.foldlevelstart = 99
+o.listchars='nbsp:␣,trail:·,extends:>,precedes:<'
+
 
 -- Set colorscheme.
 o.termguicolors = true    
